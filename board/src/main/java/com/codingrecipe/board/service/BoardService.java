d5f435c713c41cb1371e3e8e47ffa7de165996c7
imminpie <mjkang0715@gmail.com>
2023-12-15 00:12:47 +0900

    feat: 게시글 목록 기능 구현

--- board/src/main/java/com/codingrecipe/board/service/BoardService.java
+++ board/src/main/java/com/codingrecipe/board/service/BoardService.java
@@ -6,6 +6,9 @@ import com.codingrecipe.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class BoardService {
@@ -15,5 +18,18 @@ public class BoardService {
     public void save(BoardDTO boardDTO) {
         BoardEntity saveEntity = BoardEntity.toSaveEntity(boardDTO);
         boardRepository.save(saveEntity);
+    }
+
+
+    public List<BoardDTO> findAll() {
+
+        List<BoardEntity> boardEntityList = boardRepository.findAll();
+
+        // Entity → DTO
+        List<BoardDTO> boardDTOList = new ArrayList<>();
+        for (BoardEntity boardEntity : boardEntityList) {
+            boardDTOList.add(BoardDTO.toBoardDTO(boardEntity));
+        }
+        return boardDTOList;
     }
 }
